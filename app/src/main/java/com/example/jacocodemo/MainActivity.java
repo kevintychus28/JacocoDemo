4e235f13fe4f9a6287481ce16ed263b5cda911ae
tiancai.ye <tiancai.ye@dreampix.cn>
2021-05-06 15:16:59 +0800

    Add TextView

--- app/src/main/java/com/example/jacocodemo/MainActivity.java
+++ app/src/main/java/com/example/jacocodemo/MainActivity.java
@@ -4,14 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv_1;
+    Button btn_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv_1 = findViewById(R.id.tv_1);
+        btn_1 = findViewById(R.id.btn_1);
+        btn_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv_1.setText("新的了");
+            }
+        });
     }
 
     public void toast(View view) {
